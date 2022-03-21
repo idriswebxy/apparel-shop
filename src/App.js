ab557fed6756b324713ee55a9fcf5975f9035ff8
Idris Mangal <49844874+idriswebxy@users.noreply.github.com>
2022-03-21 14:46:39 -0400

    pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import "./App.css";
+import { Route, Switch, useLocation } from "react-router-dom";
+import { ProductsProvider } from './contexts/ProductsContext';
+import { CartProvider } from './contexts/CartContext';
+import Menu from './components/Menu';
+import { AnimatePresence } from "framer-motion"; 
+import HomePage from './component/pages/HomePage';
+
 
 function App() {
+  const location = useLocation();
   return (
     <div className="App">
-      
-    </div>
+      <ProductsProvider>
+        <CartProvider>
+        <Menu/>
+        <AnimatePresence>
+          <Switch>
+            <Route path="/" exact component={HomePage}/>
+
+          </Switch>
+        </AnimatePresence>
+        </CartProvider>
+      </ProductsProvider>
+    </div >
   );
 }
 
