e5cbedfcf6b3fbbcc6fbb8ed7eb7eb412fbb4516
Idris Mangal <49844874+idriswebxy@users.noreply.github.com>
2022-04-18 16:37:32 -0400

    new style changes to navbar

--- client/src/components/Menu/DrawerBox.jsx
+++ client/src/components/Menu/DrawerBox.jsx
@@ -2,7 +2,6 @@ import * as React from "react"
 import { styled, useTheme } from "@mui/material/styles"
 import Box from "@mui/material/Box"
 import Drawer from "@mui/material/Drawer"
-import CssBaseline from "@mui/material/CssBaseline"
 import MuiAppBar from "@mui/material/AppBar"
 import Toolbar from "@mui/material/Toolbar"
 import List from "@mui/material/List"
@@ -10,29 +9,14 @@ import Typography from "@mui/material/Typography"
 import Divider from "@mui/material/Divider"
 import IconButton from "@mui/material/IconButton"
 import MenuIcon from "@mui/icons-material/Menu"
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft"
-import ChevronRightIcon from "@mui/icons-material/ChevronRight"
 import ListItem from "@mui/material/ListItem"
-import ListItemIcon from "@mui/material/ListItemIcon"
 import ListItemText from "@mui/material/ListItemText"
-import InboxIcon from "@mui/icons-material/MoveToInbox"
-import MailIcon from "@mui/icons-material/Mail"
 import Button from "@mui/material/Button"
-import { Link } from "react-router-dom"
 
 const drawerWidth = 240
 
 export default function PersistentDrawerLeft() {
-  const theme = useTheme()
   const [open, setOpen] = React.useState(false)
-
-  const handleDrawerOpen = () => {
-    setOpen(true)
-  }
-
-  const handleDrawerClose = () => {
-    setOpen(false)
-  }
 
   const toggleDrawer = (open) => (event) => {
     setOpen(open)
@@ -69,15 +53,6 @@ export default function PersistentDrawerLeft() {
         duration: theme.transitions.duration.enteringScreen,
       }),
     }),
-  }))
-
-  const DrawerHeader = styled("div")(({ theme }) => ({
-    display: "flex",
-    alignItems: "center",
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-    justifyContent: "flex-end",
   }))
 
   return (
