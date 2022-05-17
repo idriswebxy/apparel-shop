2c0f082705360c2fe8dee2391bf3fc9e991500a5
Idris Mangal <49844874+idriswebxy@users.noreply.github.com>
2022-05-17 15:13:56 -0400

    Updates

--- client/src/layout/Home/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import image from "../../images/home-page.jpg"
-export default function Home() {
-  return (
-    <div>
-      <img alt="pic" src={image} height="100%" width="100%" />
-    </div>
-  )
-}
