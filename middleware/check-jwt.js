c753c3047bc2b90e063069abc05399a9003be21c
idriswebxy <icoder112@gmail.com>
2022-11-18 16:38:15 -0500

    auth updates

---
+++ middleware/check-jwt.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken")
+const config = require("config")
+
+const jwtSecret = "mysecrettoken"
+
+module.exports = function (req, res, next) {
+  //  Get token from header
+  const token = req.header("x-auth-token")
+
+  // check if no token
+  if (!token) {
+    console.log("No token...")
+    return res.status(401).json({ msg: "No token, authorization denied!" })
+  }
+
+  try {
+    const decoded = jwt.verify(token, jwtSecret)
+
+    req.user = decoded.user
+    next()
+  } catch (err) {
+    res.status(401).json({ msg: "Token is not valid..." })
+  }
+}
